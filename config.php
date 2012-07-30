b62d0a3bf92ac5859510f0dc9722c01d92ea392c
Maximilian Narr <Maxe94@live.de>
2012-07-30 22:32:00 +0200

    added modern dark style

--- config.php
+++ config.php
@@ -1,4 +1,27 @@
 <?php
+/**
+ *  This file is part of devMX TS3 Webviewer Lite.
+ *  Copyright (C) 2012 Maxe <maxe.nr@live.de>
+ *  Copyright (C) 2012 drak3 <drak3@live.de>
+ *
+ *  devMX Webviewer Lite is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  TeamSpeak3 Webviewer is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with devMX TS3 Webviewer Lite.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+
+// ### BASIC CONFIGURATION ###
+
 
 /*
  * Hostname or ip adress of your teamspeak3 server
@@ -6,11 +29,13 @@
  */
 $c['ts3']['host'] = 'localhost';
 
+
 /**
  * Queryport of your teamspeak3 server
  * default: 10011
  */
 $c['ts3']['query.port'] = 10011;
+
 
 /**
  * Serverport of the teamspeak3 server to display
@@ -18,11 +43,13 @@ $c['ts3']['query.port'] = 10011;
  */
 $c['ts3']['vserver.port'] = 9987;
 
+
 /**
  * Username of the query user [optional]
  * default: empty 
  */
 $c['ts3']['login.name'] = '';
+
 
 /**
  * Password of the query user [optional]
@@ -31,7 +58,9 @@ $c['ts3']['login.name'] = '';
 $c['ts3']['login.pass'] = '';
 
 
-//### Advanced configuration ### --> 
+
+// ### Advanced configuration ###
+
 
 /**
  * If the viewer should be cached (recommended)
@@ -39,11 +68,13 @@ $c['ts3']['login.pass'] = '';
  */
 $c['enable_caching'] = false;
 
+
 /**
  * If the viewer should download custom icons (recommended) -->
  * default: true 
  */
 $c['download_custom_icons'] = true;
+
 
 /**
  * If images should be shown (recommended)
@@ -51,11 +82,21 @@ $c['download_custom_icons'] = true;
  */
 $c['images.show'] = true;
 
+
 /**
  *  If country icons should be shown (not recommended)
  *  default: false
  */
 $c['country_icons.show'] = true;
+
+
+/**
+ * Name of the style which should be used
+ * Currently available:
+ *  - base (default style)
+ *  - modern-dark (ported from the big devMX Webviewer)
+ */
+$c['style'] = "base";
 
 
 /**
@@ -70,6 +111,7 @@ $c['render.query_clients'] = false;
  */
 $c['filter.whitelist.enabled'] = false;
 
+
 /**
  * Array of channel-ids of channels which should be rendered.
  * All other channels WONT be rendered
@@ -81,6 +123,7 @@ $c['filter.whitelist'] = array();
  * If blacklist rendering should be enabled
  */
 $c['filter.blacklist.enabled'] = false;
+
 
 /**
  * Array of channel-ids of channel which should NOT be rendered.
